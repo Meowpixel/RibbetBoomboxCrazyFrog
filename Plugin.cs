19f05fd1f89514405e5d04236e2761bf85f55a45
Henry Field <=>
2024-02-23 16:50:04 +0000

    Initial Commit

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using UnityEngine;
+using LCSoundTool;
+
+namespace RibbetBoomboxCrazyFrog
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            AudioClip crazyFrogLoop;
+            AudioClip crazyFrogFull;
+
+
+            crazyFrogLoop = SoundTool.GetAudioClip("Meowpixel-CrazyFrogBoombox", "crazyFrogLoop.mp3");
+            crazyFrogFull = SoundTool.GetAudioClip("Meowpixel-CrazyFrogBoombox", "crazyFrogFull.mp3");
+
+
+            //using a for loop to do this breaks it for some reason
+            SoundTool.ReplaceAudioClip("BoomboxMusic1", crazyFrogLoop);
+            SoundTool.ReplaceAudioClip("BoomboxMusic2", crazyFrogLoop);
+            SoundTool.ReplaceAudioClip("BoomboxMusic3", crazyFrogLoop);
+            SoundTool.ReplaceAudioClip("BoomboxMusic4", crazyFrogLoop);
+            SoundTool.ReplaceAudioClip("BoomboxMusic5Zedfox", crazyFrogFull);
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace RibbetBoomboxCrazyFrog
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "RibbetBoomboxCrazyFrog";
+        public const string PLUGIN_NAME = "RibbetBoomboxCrazyFrog";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
